124fd1be2d4f8ed9184c3d4f7166694283173f2c
moen <moenr49@gmail.com>
2025-06-21 19:29:17 +0300

    inall commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow function for square (takes one parameter)
+const square = num => num * num;
+
+// Arrow function for add (takes two parameters)
+const add = (a, b) => a + b;
